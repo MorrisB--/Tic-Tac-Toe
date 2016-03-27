62e612ba3b2783a9ffebdcfe1ff807934b739999
MorrisB-- <morris.ballenger@yahoo.com>
2016-03-27 10:30:56 -0500

    Created Gameboard class. Created userPlays(int) and computerPlays() methods. Moved printModelBoard() to Gameboard.

---
+++ src/Gameboard.java
@@ -0,0 +1,22 @@
+/** 
+ * /// ADD PROPER DESCRIPTION OF CLASS HERE \\\
+ * 
+ * @author Morris Ballenger
+ * @author // ADD NAME HERE \\
+ * @author // ADD NAME HERE \\
+ * @version 1.0
+ * @since 03-27-2016
+ * 
+ */
+
+public class Gameboard {
+	
+	public void userPlays(int l){
+		
+	}
+	
+	public void computerPlays (){
+		
+	}
+
+}
