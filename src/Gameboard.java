2a5fa3c7a0588d78dfdc24e2217be9a3fa6bf4c8
MorrisB-- <morris.ballenger@yahoo.com>
2016-03-28 11:28:56 -0500

    Added descriptions for all methods.

--- src/Gameboard.java
+++ src/Gameboard.java
@@ -17,6 +17,14 @@ public class Gameboard {
 	public Gameboard() {
 	}
 
+	/**
+	 * Finds the location that the user chose, if that location is not taken
+	 * then 'X' is played at that location, then we call checkWins(). If there
+	 * is value at that location then we tell them to choose a new location.
+	 * 
+	 * @param l
+	 *            The location that the user chose to play
+	 */
 	public void userPlays(int l) {
 
 		if (l == 0 || l == 1 || l == 2) {
@@ -40,10 +48,14 @@ public class Gameboard {
 		// print the board afterwards, something like printGameBoard() - MB
 	}
 
-	// MB
-	// 1) Check if location is empty
-	// 2) If the location is empty call checkNeighboors()
-	// 3) Play 'O' at [bestY][bestX]
+	/**
+	 * Looks through the entire board. Once it finds an empty location it calls
+	 * the bestWins() method. Once it finds the best location for the most
+	 * possible wins it will play at that location [bestY][bestX]. Then it will
+	 * call the checkWins() function. NOTE. *** This needs to first check for
+	 * the location that will allow the computer to win, then, check for the
+	 * location with the most possible wins.***
+	 */
 	public void computerPlays() {
 
 		int bestX, bestY, chances = 0;
@@ -52,7 +64,7 @@ public class Gameboard {
 			for (int j = 0; j < 2; j++) {
 				// can combine these next two statements - MB
 				if (this.array[i][j] == 0) {
-					if (bestWin(this.array, i, j) > chances){
+					if (bestWin(this.array, i, j) > chances) {
 						chances = bestWin(this.array, i, j);
 						bestX = j;
 						bestY = i;
@@ -60,10 +72,22 @@ public class Gameboard {
 				}
 			}
 		}
-		
+
 		this.checkWins();
 	}
 
+	/**
+	 * Looks in array a at location [i][j], and counts the possible wins at that
+	 * location.
+	 * 
+	 * @param a
+	 *            the array to be checked.
+	 * @param i
+	 *            the row of the location
+	 * @param j
+	 *            the column of the location
+	 * @return the number of possible wins at that location.
+	 */
 	public static int bestWin(char[][] a, int i, int j) {
 
 		return 0;
@@ -71,10 +95,16 @@ public class Gameboard {
 	}
 
 	// this may need to be boolean - MB
+	/**
+	 * Check to see if the user or computer won after their current move.
+	 */
 	public void checkWins() {
 
 	}
 
+	/**
+	 * Prints a board so the user can reference where they want to play each move.
+	 */
 	public static void printModelBoard() {
 		System.out.println("Model Board:");
 		System.out.println("  0 | 1 | 2");
